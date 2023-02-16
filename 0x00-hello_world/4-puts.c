2add9cfd22c33087e1a956cda0315d3de7ef9459
Isaiahyusuf <isaiahyusuf63@gmail.com>
2023-02-17 00:00:09 +0100

    task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - a program that print a line with puts function
+ * main - A  program that print a line with puts functions
  * Return: 0 (success)
  */
 int main(void)
