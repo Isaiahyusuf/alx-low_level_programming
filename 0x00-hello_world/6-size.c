61ceeb710364a498285bd4507cdbb59f3b1a94f3
Isaiahyusuf <isaiahyusuf63@gmail.com>
2023-02-17 14:58:38 +0100

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,6 @@
  * main - Write a C program that prints the size of various types on the computer it is compiled and run on.
  * Return 0 (success)
  */
-
 int main(void)
 {
 char a;
@@ -11,7 +10,6 @@ int b;
 long int c;
 long long int d;
 float f;
-
 printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("size of a int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
