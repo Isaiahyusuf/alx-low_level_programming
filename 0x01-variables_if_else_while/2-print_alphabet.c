3dce3b5ec301c1dc238ac22d480f968bc44d1165
Isaiahyusuf <isaiahyusuf63@gmail.com>
2023-02-18 11:43:01 +0100

    task

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,13 +3,13 @@
 /**
  * main - main block
  * Description: Get a random it with 5
- * Return 0
+ * Return: 0
  */
 int main(void)
 {
 	char c = 'a';
 
-	while (c <= 'z');
+	while (c <= 'z')
 	{
 		putchar(c);
 		c++;
