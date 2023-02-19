255720f9d78eee100120449225bce71291ab3258
Isaiahyusuf <isaiahyusuf63@gmail.com>
2023-02-19 14:22:11 +0100

    task

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib>
+/**
+ * main - main block
+ * Description: program that prints all the numbers of base 16 
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+	putchar(c);
+	c--;
+	}
+	putchar('\n');
+	return (0)
+}
