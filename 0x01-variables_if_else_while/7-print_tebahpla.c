b091a2e6d9e27d16ffa6aeeab930fba8d889866e
Isaiahyusuf <isaiahyusuf63@gmail.com>
2023-02-19 13:57:26 +0100

    rask

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include </stdlib.h>
+#include <time.h>
+/**
+ * main - main block
+ * Description: program that prints the lowercase alphabet 
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+	putchar(c);
+	c--;
+	}
+	putchar('\n');
+	return (0);
+}
