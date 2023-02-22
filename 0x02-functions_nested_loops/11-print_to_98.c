aa2c19ffd5d5e8aceb055f894884fd3f09445d0e
Isaiahyusuf <isaiahyusuf63@gmail.com>
2023-02-22 12:25:35 +0100

    tasl

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -23,9 +23,9 @@ void print_to_98(int n)
 		for (j = n; j >= 98; j--)
 		{
 			if (j != 98)
-				ptintf("%d, ", j);
+				printf("%d, ", j);
 			else if (j == 98)
-				printf("%f\n", j);
+				printf("%d\n", j);
 		}
 	}
 }
