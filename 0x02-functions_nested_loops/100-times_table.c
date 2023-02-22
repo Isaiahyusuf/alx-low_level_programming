74d7acbcf2acf5ec46b302637568433fecfcf688
Isaiahyusuf <isaiahyusuf63@gmail.com>
2023-02-22 14:06:47 +0100

    task

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,9 +9,9 @@ void print_times_table(int n)
 
 	if (n >= 0 && n <= 15)
 	{
-		for (i = 0; <=, i++)
+		for (i = 0; <= n; i++)
 		{
-			for (j = 0; j <= n, j++)
+			for (j = 0; j <= n; j++)
 			{
 				k = j * i;
 			if (i == 0)
@@ -38,10 +38,8 @@ void print_times_table(int n)
 				_putchar(((k / 10) % 10) + '0');
 				_putchat((k % 10) + '0');
 			}
-		}
-		putchar('\n');
+			}
+			putchar('\n');
 		}
 	}
 }
-
-
